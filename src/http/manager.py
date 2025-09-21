293153f9e1633a4b1639c441c51eac5df13dc130
h1kken <kirill.prokofiev3003@mail.ru>
2025-09-21 14:42:47 +0300

    update http manager

--- src/http/manager.py
+++ src/http/manager.py
@@ -1,44 +1,45 @@
 import httpx
 import atexit
 import asyncio
-from utils.logger import logger
-from exceptions.roblox import (
+from src.utils.logger import logger
+from src.exceptions.roblox import (
     InvalidCookie,
     AccountBanned
 )
 
 
 class AsyncRequestManager:
-    def __init__(self, *, headers: dict = None, cookies: dict = None):
-        self._client = httpx.AsyncClient(headers=headers, cookies=cookies)
-        atexit.register(self._cleanup)
+    def __init__(self):
+        self._client = httpx.AsyncClient()
     
     async def __aenter__(self):
         return self
 
     async def __aexit__(self, *_):
         await self.close()
-
-    def _cleanup(self):
-        try:
-            loop = asyncio.get_running_loop()
-        except RuntimeError:
-            asyncio.run(self.close())
-        else:
-            loop.create_task(self.close())
         
     async def close(self):
         await self._client.aclose()
     
-    def set_headers(self, headers: dict):
+    def update_headers(self, headers: dict):
         self._client.headers.update(headers)
     
-    def set_cookies(self, cookies: dict):
+    def update_cookies(self, cookies: dict):
         self._client.cookies.update(cookies)
     
-    async def _request(self, method: str, url: str, *, data: dict = None, json: dict = None, params: dict = None):
+    async def _request(
+        self,
+        method: str,
+        url: str,
+        *,
+        cookies: dict = None,
+        headers: dict = None,
+        data: dict = None,
+        json: dict = None,
+        params: dict = None
+    ):
         while True:
-            response = await self._client.request(method, url, data=data, json=json, params=params)
+            response = await self._client.request(method, url, cookies=cookies, headers=headers, data=data, json=json, params=params)
             match response.status_code:
                 case 200:
                     return response
@@ -54,11 +55,24 @@ class AsyncRequestManager:
                     logger.debug(f'[{response.status_code}] URL: {url}')
                     await asyncio.sleep(10)
     
-    async def get(self, url: str, *, params: dict = None):
-        return await self._request('get', url, params=params)
+    async def get(
+        self,
+        url: str,
+        *,
+        cookies: dict = None,
+        headers: dict = None,
+        params: dict = None
+    ):
+        return await self._request('get', url, cookies=cookies, headers=headers, params=params)
     
-    async def post(self, url: str, *, data: dict = None, json: dict = None, params: dict = None):
-        return await self._request('post', url, data=data, json=json, params=params)
-    
-    
-client = AsyncRequestManager()
+    async def post(
+        self,
+        url: str,
+        *,
+        cookies: dict = None,
+        headers: dict = None,
+        data: dict = None,
+        json: dict = None,
+        params: dict = None
+    ):
+        return await self._request('post', url, cookies=cookies, headers=headers, data=data, json=json, params=params)
