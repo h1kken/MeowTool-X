3ff73fff47a1fd4a291b899cd06f6b1226da46bf
h1kken <kirill.prokofiev3003@mail.ru>
2025-09-23 09:09:44 +0300

    improve request manager

--- src/http/manager.py
+++ src/http/manager.py
@@ -1,16 +1,13 @@
-import httpx
-import atexit
 import asyncio
+from httpx import AsyncClient
+
 from src.utils.logger import logger
-from src.exceptions.roblox import (
-    InvalidCookie,
-    AccountBanned
-)
+from src.exceptions.roblox import InvalidCookie, AccountBanned
 
 
 class AsyncRequestManager:
     def __init__(self):
-        self._client = httpx.AsyncClient()
+        self._client = AsyncClient()
     
     async def __aenter__(self):
         return self
@@ -32,14 +29,24 @@ class AsyncRequestManager:
         method: str,
         url: str,
         *,
-        cookies: dict = None,
-        headers: dict = None,
         data: dict = None,
         json: dict = None,
-        params: dict = None
+        params: dict = None,
+        headers: dict = None,
+        cookies: dict = None,
+        follow_redirects: bool = False
     ):
         while True:
-            response = await self._client.request(method, url, cookies=cookies, headers=headers, data=data, json=json, params=params)
+            response = await self._client.request(
+                method,
+                url,
+                params=params,
+                data=data,
+                json=json,
+                headers=headers,
+                cookies=cookies,
+                follow_redirects=follow_redirects
+            )
             match response.status_code:
                 case 200:
                     return response
@@ -59,20 +66,36 @@ class AsyncRequestManager:
         self,
         url: str,
         *,
-        cookies: dict = None,
+        params: dict = None,
         headers: dict = None,
-        params: dict = None
+        cookies: dict = None,
+        follow_redirects: bool = False
     ):
-        return await self._request('get', url, cookies=cookies, headers=headers, params=params)
+        return await self._request(
+            'get',
+            url,
+            params=params,
+            headers=headers,
+            cookies=cookies,
+            follow_redirects=follow_redirects
+        )
     
     async def post(
         self,
         url: str,
         *,
-        cookies: dict = None,
-        headers: dict = None,
+        params: dict = None,
         data: dict = None,
         json: dict = None,
-        params: dict = None
+        headers: dict = None,
+        cookies: dict = None
     ):
-        return await self._request('post', url, cookies=cookies, headers=headers, data=data, json=json, params=params)
+        return await self._request(
+            'post',
+            url,
+            params=params,
+            data=data,
+            json=json,
+            headers=headers,
+            cookies=cookies
+        )
