4128c9f00974de7275a08d37b3fce617992fb88c
h1kken <kirill.prokofiev3003@mail.ru>
2025-09-28 21:57:36 +0300

    improve config validation

--- src/config/utils.py
+++ src/config/utils.py
@@ -1,4 +1,5 @@
 import ast
+from typing import Literal
 
 def parse_config(text: str) -> dict:
     parsed = {}
@@ -28,10 +29,16 @@ def validate_config(user_config: dict, default_config: dict, *, recovery_missing
     
     for key, default_value in default_config.items():
         if key in user_config:
-            validated[key] = convert_value(user_config[key], default_value)
+            if type(user_config[key]) is dict:
+                validated[key] = validate_config(user_config[key], default_value, recovery_missing=recovery_missing)
+            else:
+                validated[key] = convert_value(user_config[key], default_value)
         else:
             if recovery_missing:
-                validated[key] = default_value
+                if type(default_value) is dict:
+                    validated[key] = validate_config({}, default_value, recovery_missing=True)
+                else:
+                    validated[key] = default_value[0] if isinstance(default_value, tuple) else default_value
 
     for key, user_value in user_config.items():
         if key not in default_config:
@@ -48,24 +55,28 @@ def literal_eval(value: str):
     except (ValueError, SyntaxError):
         return value
 
-def convert_to_bool(user_value: str):
+def convert_to_bool(user_value: str) -> bool | str:
     low = user_value.strip().lower()
-    if low in ('true', 'y', 'yes', 'on', '1', '+'):
+    if low in ('true', 'y', 'yes', 'д', 'да', 'on', '+'):
         return True
-    elif low in ('false', 'n', 'no', 'off', '0', '-'):
+    elif low in ('false', 'n', 'no', 'н', 'нет', 'off', '-'):
         return False
     else:
         return user_value
 
 def convert_value(user_value, default_value=None):
     if default_value is not None:
-        if isinstance(default_value, dict):
-            return validate_config(user_value, default_value)
-        elif isinstance(default_value, tuple):
-            if isinstance(user_value, int) and (default_value[1] <= user_value <= default_value[2]):
-                return user_value
-            else:
-                return default_value[0]
+        if isinstance(default_value, tuple):
+            if isinstance(default_value[0], int) and len(default_value) == 3:
+                if not (isinstance(user_value, int) and (default_value[1] <= user_value <= default_value[2])):
+                    return default_value[0]
+            # elif isinstance(default_value[0], str):
+            #     if isinstance(default_value[1], Literal):
+            #         if any(char in user_value for char in ['<', '>', '|', '^', '&']):
+            #             return default_value[0]
+            #     elif isinstance(default_value[1], list):
+            #         if isinstance(user_value, str) and user_value.strip().lower() not in default_value[1]:
+            #             return default_value[0]
         elif isinstance(default_value, bool):
             if isinstance(user_value, str):
                 return convert_to_bool(user_value)
