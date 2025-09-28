cafcfa06c76a60bb8d8b97968c751974327a54b3
h1kken <kirill.prokofiev3003@mail.ru>
2025-09-28 21:56:42 +0300

    improve translation initializing

--- src/translation/manager.py
+++ src/translation/manager.py
@@ -7,32 +7,49 @@ from ..utils import logger, detect_system_locale
 class TranslationManager(QObject):
     language_changed = pyqtSignal()
 
-    def __init__(self, filename: str='en'):
+    def __init__(self, filename: str = None):
         super().__init__()
-        self.translations = {}
+        self._translations = {}
         self.load_language(filename)
 
+    def find_language_path(self, filename: str) -> Path:
+        for path in (
+            Path(__file__).parent / '..' / '..' / 'Settings' / 'Translates' / f'{filename}.axis',
+            Path(__file__).parent / 'translations' / f'{filename}.axis'
+        ):
+            if path.exists():
+                return path
+            
+        logger.warning('Translation not found. Using other...')
+        return path.parent / f'{detect_system_locale()}.axis'
+
     def load_language(self, filename: str):
-        logger.info(f'[Translation] Initializing: {filename}')
-        path = Path(__file__).parent / 'Settings' / 'Translates' / f'{filename}.axis'
+        logger.info(f'Initializing translation: {filename}')
+        
+        self._path = self.find_language_path(filename)
+        config.set('General>Language', self._path.stem)
+            
         try:
-            with open(path, 'r', encoding='utf-8', errors='ignore') as f:
+            with open(self._path, 'r', encoding='utf-8', errors='ignore') as f:
                 for line in f:
                     if not line or line.startswith('!') or line.startswith('#'):
                         continue
                     
                     if '=' in line:
                         key, label = line.split('=', 1)
-                        self.translations[key.strip()] = label.strip()
+                        self._translations[key.strip()] = label.strip()
             self.language_changed.emit()
-            logger.info(f'[Translation] Initialized: {filename}')
+            logger.info(f'Translation initialized: {self._path.stem}')
         except FileNotFoundError:
-            logger.exception('[Translation] File not found')
+            logger.warning('Not found any translations. Using keys...')
         except Exception:
-            logger.exception('[Translation] Unknown error')
+            logger.exception('Translation can\'t be initialized. Unknown error:')
+
+    def create_language():
+        ...
 
     def tr(self, key: str) -> str:
-        return self.translations.get(key, key)
+        return self._translations.get(key, key)
 
 
 translator = TranslationManager(config.get('General>Language', default=detect_system_locale()))
