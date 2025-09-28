4f308b91c62e0cf4c617455d0c6fe8ee21c789b7
h1kken <kirill.prokofiev3003@mail.ru>
2025-09-28 21:59:00 +0300

    improve loader initializing

--- src/config/loader.py
+++ src/config/loader.py
@@ -7,21 +7,25 @@ from .utils import parse_config, validate_config
 
 class ConfigLoader(GetConfigMixin, SetConfigMixin, SaveConfigMixin):
     def __init__(self):
-        logger.info('Intializing loader...')
         self._path = Path('Settings', 'Configs', '.Loader.txt')
-        self._path.parent.mkdir(parents=True, exist_ok=True)
         self._data = {}
+        self._load()
         
+    def _load(self):
+        logger.info('Intializing loader...')
+        self._path.parent.mkdir(parents=True, exist_ok=True)
         try:
             with open(self._path, 'r', encoding='utf-8', errors='ignore') as f:
                 parsed_config = parse_config(f.read())
                 self._data = validate_config(parsed_config, default_config_loader())
             self.save()
+            logger.info('Loader initialized')
         except FileNotFoundError:
             logger.warning('Loader not found. Creating...')
             open(self._path, 'w').close()
-        finally:
-            logger.info('Loader initialized')
+            self._load()
+        except Exception:
+            logger.exception('Loader can\'t be initialized. Unknown error:')
             
     def set(self, key, value, *, sep='>'):
         super().set(key, value, sep=sep)
