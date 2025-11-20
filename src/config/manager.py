d7a3bed07d9a03a261be481bac65b0c4726df0c3
h1kken <kirill.prokofiev3003@mail.ru>
2025-11-21 02:51:23 +0300

    change imports, fix reset and delete methods

--- src/config/manager.py
+++ src/config/manager.py
@@ -1,19 +1,20 @@
 import os
-from pathlib import Path
-from src.utils.logger import logger
-from .defaults import default_config
-from .utils import parse_config, validate_config
-from .mixin import GetConfigMixin, SetConfigMixin, SaveConfigMixin
-from .loader import config_loader
+from src.config.defaults import default_config
+from src.config.utils import parse_config, validate_config
+from src.config.mixin import GetConfigMixin, SetConfigMixin, SaveConfigMixin
 from PyQt6.QtCore import QObject, pyqtSignal
+from src.config.loader import config_loader
+from src.utils.logger import logger
+from src.utils.consts import PATH_CONFIGS
+from src.utils.file_utils import create_folder, delete_file
 
 
-class ConfigManager(QObject, GetConfigMixin, SetConfigMixin, SaveConfigMixin):
+class Config(QObject, GetConfigMixin, SetConfigMixin, SaveConfigMixin):
     config_loaded = pyqtSignal()
     
     def __init__(self, filename: str = 'default'):
         super().__init__()
-        self._path = Path('Settings', 'Configs', f'{filename}.txt')
+        self._path = None
         self._data = {}
         self.load(filename)
         
@@ -22,26 +23,25 @@ class ConfigManager(QObject, GetConfigMixin, SetConfigMixin, SaveConfigMixin):
         return self._path.stem
     
     def create(self, filename: str):
-        if (self._path.parent / f'{filename}.txt').exists():
-            return
+        path = PATH_CONFIGS / f'{filename}.txt'
+        if path.exists(): return
         
-        self._path = self._path.parent / f'{filename}.txt'
-        self._path.parent.mkdir(parents=True, exist_ok=True)
-        open(self._path, 'w').close()
+        create_folder(PATH_CONFIGS)
+        path.touch()
         logger.info(f'Created config: {filename}')
         self.load(filename)
     
     def load(self, filename: str):
         logger.info(f'Initializing config: {filename}')
-        self._path = Path('Settings', 'Configs', f'{filename}.txt')
-        self._path.parent.mkdir(parents=True, exist_ok=True)
+        self._path = PATH_CONFIGS / f'{filename}.txt'
         
         try:
             with open(self._path, 'r', encoding='utf-8', errors='ignore') as f:
                 parsed_config = parse_config(f.read())
-                self._data = validate_config(parsed_config, default_config())
+                
+            self._data = validate_config(parsed_config, default_config())
             self.save()
-            logger.info(f'Config initialized: {self._path.stem}')
+            logger.info(f'Config initialized: {filename}')
             self.config_loaded.emit()
         except FileNotFoundError:
             logger.warning(f'Config not found. Creating...')
@@ -55,16 +55,20 @@ class ConfigManager(QObject, GetConfigMixin, SetConfigMixin, SaveConfigMixin):
             self.save()
         
     def reset(self, filename: str):
-        self._path.parent.mkdir(parents=True, exist_ok=True)
-        open(self._path.parent / f'{filename}.txt', 'w').close()
+        path = PATH_CONFIGS / f'{filename}.txt'
+        if not path.exists(): return
+        
+        create_folder(PATH_CONFIGS)
+        open(path, 'w').close()
         if self._path.stem == filename:
             self.load(filename)
             
     def delete(self, filename: str):
-        if self._path.exists():
-            os.remove(self._path.parent / f'{filename}.txt')
-            if self._path.stem == filename:
+        path = PATH_CONFIGS / f'{filename}.txt'
+        if path.exists():
+            delete_file(path)
+            if path.stem == filename:
                 self.load('default')
     
     
-config = ConfigManager(config_loader.get('Loader>Config On Launch', default='default'))
+config = Config(config_loader.get('Loader>Config On Launch', default='default'))
