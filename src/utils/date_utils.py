7b6520dd55c5bc3f62eb53d01bc5edb4eca9649e
h1kken <kirill.prokofiev3003@mail.ru>
2025-11-21 02:53:31 +0300

    change imports

--- src/utils/date_utils.py
+++ src/utils/date_utils.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+from src.utils.consts import DATE_FORMATS
 
 def current_date(output_format: str) -> str:
     return datetime.now().strftime(output_format)
@@ -6,15 +7,9 @@ def current_date(output_format: str) -> str:
 def current_time_in_ms() -> int:
     return int((datetime.now() - datetime(1970, 1, 1)).total_seconds() * 1000)
 
-DATE_FORMATS = [
-    '%Y-%m-%dT%H:%M:%S.%fZ',
-    '%Y-%m-%dT%H:%M:%SZ'
-]
-
 def convert_date(input_date: str, output_format: str) -> str:
     for date_format in DATE_FORMATS:
         try:
-            formatted_date = datetime.strptime(input_date, date_format).strftime(output_format)
-            return formatted_date
+            return datetime.strptime(input_date, date_format).strftime(output_format)
         except ValueError:
             ...
