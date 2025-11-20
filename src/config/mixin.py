e90a2a3d9d741554e419326a854d006f0f4412ca
h1kken <kirill.prokofiev3003@mail.ru>
2025-11-21 02:47:16 +0300

    change imports, variable names

--- src/config/mixin.py
+++ src/config/mixin.py
@@ -1,5 +1,7 @@
 from typing import Optional, Any
 from src.utils.file_utils import get_nested, set_nested
+from src.utils.consts import PATH_CONFIGS
+from src.utils.file_utils import create_folder
 
 
 class GetConfigMixin:
@@ -10,23 +12,23 @@ class GetConfigMixin:
 class SetConfigMixin:
     def set(self, key: str, value: Any, *, sep: str = '>'):
         set_nested(self._data, key, value, sep=sep)
-            
-            
+
+
 class SaveConfigMixin:
-    def _dump_dict(self, data: dict, indent: int = 0):
-        lines = []
-        for key, value in data.items():
+    def _dump_dict(self, old_data: dict, indent: int = 0):
+        new_data = []
+        for key, value in old_data.items():
             if isinstance(value, dict):
-                lines.append(f'{'\t' * indent}{key}')
-                lines.extend(self._dump_dict(value, indent + 1))
+                new_data.append(f'{'\t' * indent}{key}')
+                new_data.extend(self._dump_dict(value, indent + 1))
             else:
                 if type(value) is bool:
                     value = 'Yes' if value else 'No'
-                lines.append(f'{'\t' * indent}{key}: {value}')
-        return lines
+                new_data.append(f'{'\t' * indent}{key}: {value}')
+        return new_data
     
     def save(self):
+        create_folder(PATH_CONFIGS)
         lines = self._dump_dict(self._data)
-        self._path.parent.mkdir(parents=True, exist_ok=True)
         with open(self._path, 'w', encoding='utf-8') as f:
             f.write('\n'.join(lines))
