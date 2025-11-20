dcbf9c5e5d056e001c69d00748f345bcd942082b
h1kken <kirill.prokofiev3003@mail.ru>
2025-11-21 02:51:52 +0300

    cleaning

--- src/utils/other_utils.py
+++ src/utils/other_utils.py
@@ -1,13 +1,4 @@
 import os
-from typing import Literal
-import locale
 
 def cls() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
-
-def detect_system_locale() -> Literal['RU', 'EN']:
-    system_locale = locale.getlocale()[0].lower()
-    if 'russia' in system_locale:
-        return 'RU'
-    else:
-        return 'EN'
