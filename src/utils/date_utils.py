a8a74b57eb774118961903e6d6587b78392a4115
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-01 14:50:50 +0300

    add constant to date function

--- src/utils/date_utils.py
+++ src/utils/date_utils.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from src.utils.consts import DATE_FORMATS
+from src.utils.consts import DATE_ROBLOX_FORMATS
+
 
 def current_date(output_format: str) -> str:
     return datetime.now().strftime(output_format)
@@ -8,7 +9,7 @@ def current_time_in_ms() -> int:
     return int((datetime.now() - datetime(1970, 1, 1)).total_seconds() * 1000)
 
 def convert_date(input_date: str, output_format: str) -> str:
-    for date_format in DATE_FORMATS:
+    for date_format in DATE_ROBLOX_FORMATS:
         try:
             return datetime.strptime(input_date, date_format).strftime(output_format)
         except ValueError:
