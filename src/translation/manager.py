c51bda789adc1f50034e5e43d70664f06ba855f2
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-30 02:41:42 +0300

    logging, minor changes

--- src/translation/manager.py
+++ src/translation/manager.py
@@ -1,7 +1,7 @@
-import shutil
 from pathlib import Path
-from PyQt6.QtCore import QObject, pyqtSignal
+from PySide6.QtCore import QObject, Signal
 from src.config.manager import config
+from src.utils.pyside6 import emit
 from src.utils.logger import logger
 from src.utils.consts import (
     PATH_TRANSLATIONS_USER,
@@ -9,11 +9,11 @@ from src.utils.consts import (
     SYSTEM_LOCALE,
     CONFIG_COMMENT_SYMBOLS
 )
-from src.utils.file_utils import create_folder
+from src.utils.file import create_folder, copy_file
 
 
 class TranslationManager(QObject):
-    language_changed = pyqtSignal()
+    language_changed = Signal()
 
     def __init__(self, filename: str = None):
         super().__init__()
@@ -35,8 +35,8 @@ class TranslationManager(QObject):
                 return path
             
         logger.warning(f'Translation not found. Using default: {SYSTEM_LOCALE}')
-        config.set('General>Language', SYSTEM_LOCALE)
-        return path.parent / f'{SYSTEM_LOCALE.lower()}.axis'
+        # config.set('General>Language', SYSTEM_LOCALE)
+        return PATH_TRANSLATIONS_SOURCE / f'{SYSTEM_LOCALE.lower()}.axis'
 
     def load_language(self, filename: str) -> None:
         logger.info(f'Initializing translation: {filename}')
@@ -48,22 +48,24 @@ class TranslationManager(QObject):
                     if line and line[0] not in CONFIG_COMMENT_SYMBOLS and '=' in line:
                         key, label = line.split('=', 1)
                         self._translations[key.strip()] = label.strip()
-            self.language_changed.emit()
-            logger.info(f'Translation initialized: {self._path.stem}')
+            emit(self.language_changed)
+            logger.info(f'Translation initialized: {self.name}')
         except FileNotFoundError:
             logger.warning('Not found any translations. Using keys...')
-        except Exception:
-            logger.exception('Translation can\'t be initialized. Unknown error:')
+        except Exception as e:
+            logger.exception(f'Translation can\'t be initialized. Error: {e}')
 
     def create_my_own_language(self, to_language: str, from_language: str) -> None:
-        new_language = PATH_TRANSLATIONS_USER / f'{to_language}.axis'
-        if new_language.exists(): return
+        new_path = PATH_TRANSLATIONS_USER / f'{to_language}.axis'
+        if new_path.exists():
+            return
 
-        old_language = PATH_TRANSLATIONS_SOURCE / f'{from_language}.axis'
-        if not old_language.exists(): return
+        old_path = PATH_TRANSLATIONS_SOURCE / f'{from_language}.axis'
+        if not old_path.exists():
+            return
         
         create_folder(PATH_TRANSLATIONS_USER)
-        shutil.copy(str(old_language), str(new_language))
+        copy_file(old_path, new_path)
 
     def tr(self, key: str) -> str:
         return self._translations.get(key, key)
