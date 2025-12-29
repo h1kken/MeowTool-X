0d56b001ef2d824f0589ff7d24a9f3df54c7aa8b
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-30 01:23:00 +0300

    init ansi.py | ansi codes like colors, decors, etc.

---
+++ src/utils/ansi.py
@@ -0,0 +1,22 @@
+# colors
+RED = '\x1b[31m'
+GREEN = '\x1b[32m'
+YELLOW = '\x1b[33m'
+BLUE = '\x1b[34m'
+PURPLE = '\x1b[35m'
+CYAN = '\x1b[36m'
+GRAY = '\x1b[90m'
+PINK = '\x1b[95m'
+
+# light colors
+LIGHTRED = '\x1b[91m'
+LIGHTGREEN = '\x1b[92m'
+LIGHTYELLOW = '\x1b[93m'
+LIGHTBLUE = '\x1b[94m'
+LIGHTCYAN = '\x1b[96m'
+
+# decors
+BOLD = '\x1b[1m'
+
+# other
+CLEAR = '\x1b[0m'
