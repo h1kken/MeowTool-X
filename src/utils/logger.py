d1ab4ab5c85bfe77a8ca3a983afacd461d0cdaba
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-30 02:42:40 +0300

    reworked logger module

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -1,48 +1,86 @@
-import logging
-from logging.handlers import RotatingFileHandler
+import sys
+import loguru
+from datetime import datetime
 from pathlib import Path
-from src.utils.date_utils import current_date
-from src.utils.consts import IS_LAUNCHED_IN_CONSOLE, DATE_LOGGER_FORMAT
+from src.utils.consts import IS_LAUNCHED_WITH_CONSOLE, PATH_LOGGER
+from src.utils.ansi import (
+    RED,
+    YELLOW,
+    LIGHTGREEN,
+    LIGHTYELLOW,
+    LIGHTCYAN,
+    CLEAR
+)
+
+
+def patcher(record: dict):
+    name = record.get('name')
+    if isinstance(name, str) and name.startswith('src.'):
+        record['name'] = name.removeprefix('src.')
 
 
 class Logger:
     def __init__(
         self,
-        name: str,
         *,
-        stream: bool = IS_LAUNCHED_IN_CONSOLE,
-        level: int = logging.DEBUG
+        stream: bool = IS_LAUNCHED_WITH_CONSOLE,
+        level: str = 'DEBUG'
     ):
-        self._logger = logging.getLogger(name)
-        self._logger.setLevel(level)
-        self._path = Path('Logs', f'{name} ({current_date(DATE_LOGGER_FORMAT)}).log')
-        self._path.parent.mkdir(parents=True, exist_ok=True)
+        self._logger = loguru.logger
+        self._logger.remove()
         
-        if not self._logger.handlers:
-            formatter = logging.Formatter('%(asctime)s > [%(name)s] > %(levelname)s | %(message)s')
+        self._logger.configure(patcher=patcher)
+        
+        self._path = PATH_LOGGER
+        self._path.parent.mkdir(parents=True, exist_ok=True)
 
-            if stream:
-                console_handler = logging.StreamHandler()
-                console_handler.setLevel(logging.DEBUG)
-                console_handler.setFormatter(formatter)
-                self._logger.addHandler(console_handler)
-                self._logger.debug('[!] PROGRAM IS LAUNCHED IN TESTING MODE [!]')
-            
-            file_handler = RotatingFileHandler(
-                filename=self._path,
-                maxBytes=1024*1024*5,
-                backupCount=5,
-                encoding='utf-8'
+        if stream:
+            _logger_console_format = (
+                f'{LIGHTGREEN}'
+                '{time:HH:mm:ss.SSS}'
+                f' {YELLOW}| {LIGHTCYAN}'
+                '{name:>25}'
+                f'{CLEAR}:{LIGHTCYAN}'
+                '{line:<4}'
+                f' {YELLOW}| {LIGHTYELLOW}'
+                '{function:<20}'
+                f' {YELLOW}| {CLEAR}'
+                '<level>{level:<7}</level>'
+                f' {YELLOW}| {CLEAR}'
+                '<level>{message}</level>'
             )
-            file_handler.setFormatter(formatter)
-            file_handler.setLevel(logging.DEBUG)
-            self._logger.addHandler(file_handler)
+            
+            self._logger.add(
+                sink=sys.stdout,
+                level=level,
+                format=_logger_console_format,
+                colorize=True
+            )
+            self._logger.debug(f'{RED}[!] PROGRAM IS LAUNCHED IN TESTING MODE [!]{CLEAR}')
 
-    def debug(self, message: str = '') -> None: self._logger.debug(message)
-    def info(self, message: str = '') -> None: self._logger.info(message)
-    def warning(self, message: str = '') -> None: self._logger.warning(message)
-    def error(self, message: str = '') -> None: self._logger.error(message)
-    def exception(self, message: str = '') -> None: self._logger.exception(message)
+        _logger_file_format = (
+            '{time:HH:mm:ss.SSS} | '
+            '{name:>25}:{line:<4} | '
+            '{function:>20} | '
+            '{level:<7} | '
+            '{message}'
+        )
+        
+        self._logger.add(
+            self._path,
+            level=level,
+            format=_logger_file_format,
+            rotation='5 MB',
+            retention=3,
+            encoding='utf-8',
+            enqueue=True
+        )
+
+    def debug(self, message: str = '') -> None: self._logger.opt(depth=1).debug(message)
+    def info(self, message: str = '') -> None: self._logger.opt(depth=1).info(message)
+    def warning(self, message: str = '') -> None: self._logger.opt(depth=1).warning(message)
+    def error(self, message: str = '') -> None: self._logger.opt(depth=1).error(message)
+    def exception(self, message: str = '') -> None: self._logger.opt(depth=1, exception=True).error(message)
 
 
-logger = Logger('MeowTool')
+logger = Logger()
