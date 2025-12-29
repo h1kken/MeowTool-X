fa97d580b48c93e63b9922909aca7198906c6b46
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-30 02:43:22 +0300

    minor changes

--- src/config/manager.py
+++ src/config/manager.py
@@ -1,16 +1,16 @@
-import os
+from PySide6.QtCore import QObject, Signal
 from src.config.defaults import default_config
 from src.config.utils import parse_config, validate_config
 from src.config.mixin import GetConfigMixin, SetConfigMixin, SaveConfigMixin
-from PyQt6.QtCore import QObject, pyqtSignal
 from src.config.loader import config_loader
+from src.utils.pyside6 import emit
 from src.utils.logger import logger
 from src.utils.consts import PATH_CONFIGS
-from src.utils.file_utils import create_folder, delete_file
+from src.utils.file import create_folder, create_file, create_clean_file, delete_file
 
 
 class Config(QObject, GetConfigMixin, SetConfigMixin, SaveConfigMixin):
-    config_loaded = pyqtSignal()
+    config_loaded = Signal()
     
     def __init__(self, filename: str = 'default'):
         super().__init__()
@@ -21,12 +21,12 @@ class Config(QObject, GetConfigMixin, SetConfigMixin, SaveConfigMixin):
     def name(self):
         return self._path.stem
     
-    def create(self, filename: str):
+    def create_config(self, filename: str):
         path = PATH_CONFIGS / f'{filename}.txt'
         if path.exists(): return
         
         create_folder(PATH_CONFIGS)
-        path.touch()
+        create_file(path)
         logger.info(f'Created config: {filename}')
         self.load(filename)
     
@@ -37,16 +37,16 @@ class Config(QObject, GetConfigMixin, SetConfigMixin, SaveConfigMixin):
         try:
             with open(self._path, 'r', encoding='utf-8', errors='ignore') as f:
                 parsed_config = parse_config(f.read())
-                
+            
             self._data = validate_config(parsed_config, default_config())
             self.save()
             logger.info(f'Config initialized: {filename}')
-            self.config_loaded.emit()
+            emit(self.config_loaded)
         except FileNotFoundError:
             logger.warning(f'Config not found. Creating...')
-            self.create(filename)
-        except Exception:
-            logger.exception('Config can\'t be initialized. Unknown error:')
+            self.create_config(filename)
+        except Exception as e:
+            logger.exception(f'Config can\'t be initialized. Error: {e}')
         
     def set(self, key, value, *, sep='>', force_save: bool = False):
         super().set(key, value, sep=sep)
@@ -55,19 +55,19 @@ class Config(QObject, GetConfigMixin, SetConfigMixin, SaveConfigMixin):
         
     def reset(self, filename: str):
         path = PATH_CONFIGS / f'{filename}.txt'
-        if not path.exists(): return
+        if not path.exists():
+            return
         
         create_folder(PATH_CONFIGS)
-        open(path, 'w').close()
-        if self._path.stem == filename:
+        create_clean_file(path)
+        if self.name == filename:
             self.load(filename)
             
     def delete(self, filename: str):
         path = PATH_CONFIGS / f'{filename}.txt'
-        if path.exists():
-            delete_file(path)
-            if path.stem == filename:
-                self.load('default')
+        delete_file(path)
+        if self.name == filename:
+            self.load('default')
     
     
 config = Config(config_loader.get('Loader>Config On Launch', default='default'))
