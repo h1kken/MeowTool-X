e624786f857436ae6fad2614151d98239e21f05e
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-30 01:26:10 +0300

    add SIGNAL_NAME_PATTERN, rework ROBLOX_AGE_GROUP_PATTERN

---
+++ src/utils/regex.py
@@ -0,0 +1,26 @@
+import re
+
+
+ROBLOX_COOKIE_PATTERN = re.compile(
+    r'_\|(?:_|[^\s\r\n]*?\|_)\S{100,}'
+)
+
+ROBLOX_VERSION_PATH_PATTERN = re.compile(
+    r'version-.+'
+)
+
+ROBLOX_AGE_GROUP_PATTERN = re.compile(
+    r'(?i)(Over|Under)?(\d+)[^\d]*(\d+)?'
+)
+
+STRING_100_PLUS_SYMBOLS_PATTERN = re.compile(
+    r'\S{100,}'
+)
+
+FILENAME_SPECIAL_CHARS_PATTERN = re.compile(
+    r'[\\/*?:"<>|]'
+)
+
+SIGNAL_NAME_PATTERN = re.compile(
+    r'SignalInstance (\w+)\(\)'
+)
