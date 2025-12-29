931696f8df98db67b58a53eea11748a048cf5a09
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-30 01:30:19 +0300

    init pyside6.py | logging emitter

---
+++ src/utils/pyside6.py
@@ -0,0 +1,10 @@
+import re
+from src.utils.logger import logger
+from src.utils.regex import SIGNAL_NAME_PATTERN
+
+
+def emit(signal, *args):
+    match = re.search(SIGNAL_NAME_PATTERN, str(signal))
+    logger_method, signal_name = [logger.debug, match.group(1)] if match else [logger.warning, 'Unknown Signal']
+    logger_method(f'Emitting \'{signal_name}\'. Args: {args}')
+    signal.emit(*args)
