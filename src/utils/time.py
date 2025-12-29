e48d86c8157d77c8de421150246795b7ede747ff
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-30 01:25:12 +0300

    rework format_duration function

---
+++ src/utils/time.py
@@ -0,0 +1,21 @@
+from collections.abc import Collection
+
+
+def format_duration(
+    ms: int,
+    *,
+    out_units: str | Collection[str] = 'all'
+) -> dict[str, int]:
+    s, ms = divmod(ms, 1000)
+    m, s  = divmod(s,  60)
+    h, m  = divmod(m,  60)
+    d, h  = divmod(h,  24)
+    
+    units = {'d': d, 'h': h, 'm': m, 's': s, 'ms': ms}
+    
+    parts = {}
+    for key in units.keys():
+        if key in out_units or (isinstance(out_units, str) and 'all' == out_units):
+            parts[key] = units[key]
+
+    return parts
