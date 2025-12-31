b92b14f0ca1de40e4c1bdc76813db0a492b3e6f0
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-31 17:59:41 +0300

    add beep function, rename chunked function

--- src/utils/other.py
+++ src/utils/other.py
@@ -1,9 +1,17 @@
 import os
+import sys
+from typing import Generator, TypeVar
 
 
 def cls() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
     
-def chunks_generator(lst: list, n: int):
-    for i in range(0, len(lst), n):
-        yield lst[i:i + n]
+def beep() -> None:
+    sys.stdout.write('\a')
+    sys.stdout.flush()
+
+T = TypeVar('T')
+
+def chunked(seq: list[T], size: int) -> Generator[list[T], None, None]:
+    for i in range(0, len(seq), size):
+        yield seq[i:i + size]
