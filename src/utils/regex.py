69c2d6cb919d1afb6c70f051a345338331949bea
h1kken <kirill.prokofiev3003@mail.ru>
2025-12-31 17:51:19 +0300

    add proxy patterns

--- src/utils/regex.py
+++ src/utils/regex.py
@@ -24,3 +24,29 @@ FILENAME_SPECIAL_CHARS_PATTERN = re.compile(
 SIGNAL_NAME_PATTERN = re.compile(
     r'SignalInstance (\w+)\(\)'
 )
+
+PROXY_PROTOCOL_PATTERN = re.compile(
+    r'(?i)^(https?|socks[45])'
+)
+
+PROXY_PROTOCOL_IP_PORT_USER_PASS_PATTERN = re.compile(
+    r'(?i)^(?:(?P<protocol>https?|socks[45])://)?'
+    r'(?P<ip>[^:]+):'
+    r'(?P<port>\d{1,5}):'
+    r'(?P<username>[^:]+):'
+    r'(?P<password>.+)$'
+)
+
+PROXY_PROTOCOL_USER_PASS_IP_PORT_PATTERN = re.compile(
+    r'(?i)^(?:(?P<protocol>https?|socks[45])://)?'
+    r'(?P<username>[^:@]+):'
+    r'(?P<password>[^:@]+)@'
+    r'(?P<ip>[^:]+):'
+    r'(?P<port>\d{1,5})$'
+)
+
+PROXY_PROTOCOL_IP_PORT_PATTERN = re.compile(
+    r'(?i)^(?:(?P<protocol>https?|socks[45])://)?'
+    r'(?P<ip>[^:]+):'
+    r'(?P<port>\d{1,5})$'
+)
