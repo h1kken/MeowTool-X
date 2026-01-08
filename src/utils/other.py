2d9f29ddf92d8e0dcf809b22976afd7420388189
h1kken <kirill.prokofiev3003@mail.ru>
2026-01-08 22:05:08 +0300

    minor changes

--- src/utils/other.py
+++ src/utils/other.py
@@ -1,16 +1,17 @@
 import os
 import sys
-from typing import Generator, TypeVar
+from typing import Generator
+from src.utils.types import T
 
 
 def cls() -> None:
     os.system('cls' if os.name == 'nt' else 'clear')
-    
+
+
 def beep() -> None:
     sys.stdout.write('\a')
     sys.stdout.flush()
 
-T = TypeVar('T')
 
 def chunked(seq: list[T], size: int) -> Generator[list[T], None, None]:
     for i in range(0, len(seq), size):
