b65790943d2930c7c05e88547d0f5d6d38546d29
h1kken <kirill.prokofiev3003@mail.ru>
2026-01-29 10:56:55 +0300

    add connect function

--- src/utils/pyside6.py
+++ src/utils/pyside6.py
@@ -1,10 +1,20 @@
 import re
+from typing import Optional, Callable
+from PySide6.QtCore import Signal
 from src.utils.logging import logger
 from src.utils.regex import SIGNAL_NAME_PATTERN
 
 
-def emit(signal, *args):
+def connect(signal: Signal, *, func: Optional[Callable] = None):
     match = re.search(SIGNAL_NAME_PATTERN, str(signal))
-    logger_method, signal_name = [logger.debug, match.group(1)] if match else [logger.warning, 'Unknown Signal']
-    logger_method(f'Emitting \'{signal_name}\'. Args: {args}')
+    signal_name = match.group(1) if match else 'Unknown Signal'
+    func_name = getattr(func, '__qualname__', repr(func))
+    logger.debug(f'Connecting \'{signal_name}\' to \'{func_name}\'')
+    signal.connect(func)
+
+
+def emit(signal: Signal, *args):
+    match = re.search(SIGNAL_NAME_PATTERN, str(signal))
+    signal_name = match.group(1) if match else 'Unknown Signal'
+    logger.debug(f'Emitting \'{signal_name}\'. Args: {args}')
     signal.emit(*args)
