4abe6413d214fec0bcc7d5b8a567a19abba6759a
h1kken <kirill.prokofiev3003@mail.ru>
2026-01-29 11:11:42 +0300

    rename

--- src/ui/settings/settings_window.py
+++
@@ -1,9 +0,0 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout
-from src.ui.widgets.custom_widgets import MTLabel
-
-
-class SettingsWindow(QWidget):
-    def __init__(self):
-        super().__init__()
-        layout = QVBoxLayout(self)
-        layout.addWidget(MTLabel('Настройки'))
